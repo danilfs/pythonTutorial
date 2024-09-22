de3f0abfdcd2a0ddf8557883e5af34d0fadf447f
Danil <danilfs009@yandex.ru>
2024-09-22 15:05:10 +0300

    finished lesson 18

---
+++ _17_import/lesson_17.py
@@ -0,0 +1,13 @@
+import random
+from math_operations import add
+
+# my_list = [1,2,3]
+#
+# print(random.choice(my_list))
+# print(globals().keys())
+
+# print(dir(random))
+# print (add(1,2))
+# print(subtract(9,2))
+
+# print(add(1,5))
