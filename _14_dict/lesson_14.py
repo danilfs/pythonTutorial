e6c98416571302439b18c5da7bc42526d82df654
Danil <danilfs009@yandex.ru>
2025-01-18 18:27:30 +0300

    set and get

--- _14_dict/lesson_14.py
+++ _14_dict/lesson_14.py
@@ -1,17 +1,44 @@
-person = {
-	"name": "John",
-	"age": 30,
-	"city": "New-York"
-}
-
-for value in person.values():
-	print(value)
-
-
-
-
-# person = {}
+# person = {
+#     "name": "John",
+#     "age": 30,
+#     "city": "New York"
+# }
+# print(person)  # Outputs: {'name': 'John', 'age': 30, 'city': 'New York'}
+# person["job"] = "Engineer"
+# print(person)  # Outputs: {'name': 'John', 'age': 40, 'city': 'New York', 'job': 'Engineer'}
 #
+# person = {}  # or person = dict()
 # person["name"] = "John"
+# person["age"] = 30
+# person["city"] = "New York"
+# print(person)  # Outputs: {'name': 'John', 'age': 30, 'city': 'New York'}
+#
+# person = {
+#     "name": "John",
+#     "age": 30,
+#     "city": "New York"
+# }
+# print(person["name"])  # Outputs: 'John'
+# print(person.get("name"))  # Outputs: 'John'
+# print(person.get("country"))  # Outputs: None
+# print(person.get("country", "USA"))  # Outputs: USA
+# print(person.get("name", "Jack"))  # Outputs: John
+
+# ---------------------------------------------------------------
+
+# person = {
+# 	"name": "Danil",
+# 	"age": 32,
+# 	"city":"SPB"
+# }
 #
 # print(person)
+# person["job"] = "Engineer"
+# print(person)
+
+person = {}
+# or person = dict()
+person["name"] = "Danil"
+person["city"] = "SPB"
+person["age"] = 32
+print(person)
