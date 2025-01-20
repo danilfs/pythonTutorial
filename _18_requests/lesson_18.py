86454fa4c8bf6380b86ae73df054ff59d9f3f6c5
Danil <danilfs009@yandex.ru>
2025-01-20 14:19:58 +0300

    min, max of requested price

--- _18_requests/lesson_18.py
+++ _18_requests/lesson_18.py
@@ -1,11 +1,26 @@
-import requests
-from urllib3 import request
+import json
+import time
 
-responce = requests.get('https://api.binance.com/api/v3/ticker/price', params= {'symbol':'BTCUSDT'})
-content = responce.content
-print(content)
-print(type(content))
-price_object = responce.json()
-print(price_object)
-price = price_object["price"]
-print(price)
+import requests
+
+# response = requests.get('https://api.binance.com/api/v3/ticker/price', params={'symbol': 'BTCUSDT'})
+# content = response.content
+# print(content)
+# price_object = response.json()
+# print(price_object)
+# price = float(price_object['price'])
+# print(price)
+
+bitcoin_prices = []
+
+for i in range(30):
+	response = requests.get('https://api.binance.com/api/v3/ticker/price', params= {'symbol':'BTCUSDT'})
+	price_object = response.json()
+	price = float(price_object['price'])
+	bitcoin_prices.append(price)
+
+
+print(bitcoin_prices)
+print(len(bitcoin_prices))
+print(max(bitcoin_prices))
+print(min(bitcoin_prices))
